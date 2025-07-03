ddbca84a1218b45a7fbf5771886035ac1ba4aae0
Jabir-05 <knock5jabir@gmail.com>
2025-07-03 16:48:40 +0530

    Day2

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
-import './Hero.css';
-import hand_icon from '../../assets/hand_icon.png';
-import arrow_icon from '../../assets/arrow_icon.png';
-import men from '../../assets/men.webp';
-
-export default function Hero() {
+import React from 'react'
+import "./Hero.css"
+import hand_icon from "../../assets/hand_icon.png"
+import arrow_icon from "../../assets/arrow_icon.png"
+import men from "../../assets/men.webp"
+const Hero = () => {
   return (
     <div className='hero'>
-      <div className="left">
-        <h2>Best Deals! Best Prices</h2>
-      </div>
-
-      <div className="hand-hand-icon">
-        <p>new</p>
-        <img src={hand_icon} alt="hand_icon" height="50px" />
-     
-      <p>Collections</p>
-      <p>For Everyones</p>
- </div>
- <div className="hero-latest-btn">
-    <div>Latest Collection</div>
-    <img src={arrow_icon} alt="arrow_icon" height="20px" />
- </div>
-
-      <div className="right">
-        <h2>
-            <img src={men} alt="" height="200px" />
-        </h2>
-      </div>
+        <div className="hero-left">
+<h2>Best Deals !  Best Prices !</h2>
+       
+        <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" height="60px" />
+        </div>
+        <p>collections</p>
+        <p>for everyone</p>
+        <div className="hero-latest-btn">
+            <div>Latest Collection</div>
+            <img src={arrow_icon} alt="" height="30px"/>
+        </div>
+        </div>
+        
+        <div className="hero-right">
+  <img src={men} alt="" height="400px" />
+        </div>
     </div>
-  );
+  )
 }
+
+export default Hero
